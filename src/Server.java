298cafc5d79446e8f5345646b2b21c44fe949d98
James Wen <jrw2175@columbia.edu>
2013-10-02 19:40:41 -0400

    Basic server functionality (asking user for port and creating server socket implemented)

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,14 @@
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Server {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) throws IOException{
 		Scanner passwordParse = new Scanner(new FileReader("passwords.txt"));
-		String line = "";
+		Scanner input = new Scanner(System.in);
 		String userName = "";
 		String password = "";
 		Account newAcc;
@@ -24,9 +20,41 @@ public class Server {
 			accounts.add(newAcc);
 		}
 		for(Account acc : accounts){
-			System.out.println(acc);
+			//System.out.println(acc);
 		}
-
+		int serverPort = 0;
+		boolean successfulPort = false;
+		do {
+			System.out.println("Which port would you like the server to listen in on?");
+			String desiredPort = input.nextLine();
+			try{
+				serverPort = Integer.parseInt(desiredPort.trim());
+				successfulPort = true;
+			}
+			catch(NumberFormatException e) { 
+			        successfulPort = false;
+			}
+		} while (!successfulPort);
+		
+		ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(serverPort);
+        } 
+        catch (IOException e) {
+            System.err.println("Can not listen on port: " + serverPort);
+            System.exit(1);
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println("Can not listen on port: " + serverPort);
+            System.exit(1);
+        }
+        System.out.println("Server is up and listening on port " + serverPort);
+        
+        while(true){
+        	
+        }
+        
+        
 	}
 
 }
