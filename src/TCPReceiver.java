d879588347a83d3f081dc0484b43fbbe102d5470
James Wen <jrw2175@columbia.edu>
2013-11-11 00:20:55 -0500

    Corruption checking implemented

--- src/TCPReceiver.java
+++ src/TCPReceiver.java
@@ -6,6 +6,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 
@@ -17,6 +18,8 @@ public class TCPReceiver {
 	private int remotePort; //server port to receive 
 	private String logFileName;
 	private DatagramSocket packetSocket;
+	private final int HEADERSIZE = 20;
+	private final int CHECKSUMSIZE = 5;
 	
 	private DatagramPacket bufferPacket;
 	private byte[] buffer;
@@ -36,30 +39,62 @@ public class TCPReceiver {
 			e.printStackTrace();
 		}
 	}
-	
+	private boolean corruptCheck(byte[] packetData) throws NoSuchAlgorithmException{
+		boolean packetCorrupt;
+		byte[] calculatedCheckSum = new byte[CHECKSUMSIZE];
+		byte[] actualCheckSum = new byte[CHECKSUMSIZE];
+		byte[] entireCalcCheckSum;
+		byte[] queryArray;
+		
+		//Generate byte array of all headers other than checksum and data
+		queryArray = new byte[(HEADERSIZE -  CHECKSUMSIZE) + getDataLength(packetData)];
+		for(int i = 0; i < (HEADERSIZE -  CHECKSUMSIZE); i++){
+			queryArray[i] = packetData[i];
+		}
+		for(int i = (HEADERSIZE -  CHECKSUMSIZE); i < queryArray.length; i++){
+			queryArray[i] = packetData[i + (CHECKSUMSIZE)];
+		}
+		
+		//Calculate check sum of what's been received
+		entireCalcCheckSum = Packet.generateCheckSum(queryArray);
+		
+		//Collect actual check sum and calculated check sum (first 5 bytes)
+		for(int i = (HEADERSIZE -  CHECKSUMSIZE); i < HEADERSIZE; i++){
+			actualCheckSum[i - (HEADERSIZE -  CHECKSUMSIZE)] = packetData[i];
+			calculatedCheckSum[i - (HEADERSIZE -  CHECKSUMSIZE)] = entireCalcCheckSum[i - (HEADERSIZE -  CHECKSUMSIZE)];
+		}
+		packetCorrupt = !Packet.compareCheckSums(calculatedCheckSum, actualCheckSum);
+		return packetCorrupt;
+	}
+
 	public void receive(){
 		boolean tcpComplete = false;
 		ArrayList<byte[]> fileParts = new ArrayList<byte[]>();
-		while(!tcpComplete){
-			try {
+		try {	
+			while(!tcpComplete){	
 				packetSocket.receive(bufferPacket);
 				if (Packet.getPurpose(bufferPacket.getData()[12]) == "FIN"){
 					tcpComplete = true;
 				}
+				else if (corruptCheck(bufferPacket.getData())){//is corrupt data packet
+					//send corrupt ACK
+					
+				}
 				else{//Packet is not corrupt and is correct one
 					fileParts.add(bufferPacket.getData());
-					//flush buffer and packet
-					this.buffer = new byte[576];
-					this.bufferPacket = new DatagramPacket(buffer, buffer.length);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			//if packet is corrupt
-			  //send corrupt ACK
-		    //if packet is fine
-			  //send regular ACK
+					
+					//send regular ACK
+				}				
+				//flush buffer and packet
+				this.buffer = new byte[576];
+				this.bufferPacket = new DatagramPacket(buffer, buffer.length);
+			}   
 		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}	
 		compileFile(fileParts);
 		//Print stats
 		packetSocket.close();
@@ -72,7 +107,7 @@ public class TCPReceiver {
 		completeFileBuffer = new byte[fileLength];
 		int fileFilled = 0;
 		for(byte[] buff : filePortions){
-            for(int i = 20; i < getDataLength(buff) + 20; i++){
+            for(int i = HEADERSIZE; i < getDataLength(buff) + HEADERSIZE; i++){
 				completeFileBuffer[fileFilled] = buff[i];
 				fileFilled++;
 			}
